95ce820ac285c68c060eb4b3d3b6943b403e6014
Pavel Bliznakov <pavel@vgorata.com>
2026-09-24 08:20:21 +0300

    Fix database permissions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-service nginx start
 source config/default
+service nginx start
 python3 utils/init_db.py
+# uWSGI runs as www-data. SQLite needs write access to the database and to its
+# directory (journal file). Grant it through the group to keep host ownership.
+STATIONS_DB_DIR=$(dirname $STATIONS_DB)
+chgrp www-data $STATIONS_DB_DIR $STATIONS_DB
+chmod g+w $STATIONS_DB_DIR $STATIONS_DB
 uwsgi --uid www-data uwsgi_windy_transform.ini
